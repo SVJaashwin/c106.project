8da1e0ba8108af6595be296f08dc603851818c6f
SVJaashwin <88593780+SVJaashwin@users.noreply.github.com>
2022-02-05 09:13:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 //https://teachablemachine.withgoogle.com/models/iI4XuVyLL/
+
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({
+        audio:true
+    });
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/iI4XuVyLL/model.json",modelReady);
+  
+}
+function modelReady(){
+  classifier.classify(gotResults);
+}
+function gotResults(error,results){
+  console.log("got results");
+  console.log(results);
+}
